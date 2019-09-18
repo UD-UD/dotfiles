81544f3a1ddc8c14e5d50857d0a820c76275e0a9
Ujjal Kumar Dutta <duttaujjal143@gmail.com>
2019-09-18 19:06:11 +0530

    default zsh

--- init.sh
+++ init.sh
@@ -50,7 +50,7 @@ esac
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # using Pure Prompt
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
