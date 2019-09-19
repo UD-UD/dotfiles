448da79e3a108ed5681baff52fecd8a2ff2f588e
Ujjal Kumar Dutta <duttaujjal143@gmail.com>
2019-09-19 15:50:46 +0530

    Vim Integration

--- init.sh
+++ init.sh
@@ -97,6 +97,8 @@ ln -s ~/dotfiles/configs/zshrc ~/.zshrc
 ln -s ~/dotfiles/configs/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/configs/nvim/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/configs/nvim ~/.config/nvim
+ln -s ~/dotfiles/configs/nvim/init.vim ~/.vimrc
+ln -s ~/dotfiles/configs/nvim ~/.config/.vim
 ln -sf $HOME/.zsh/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
 
 echo "Dot files installed successfully."
