984c79029291a0c25fea66c3a799d5558f81a783
Ujjal Kumar Dutta <duttaujjal143@gmail.com>
2019-09-22 16:43:24 +0530

    Refactor

--- install/brew.sh
+++ install/brew.sh
@@ -25,7 +25,7 @@ cask_formulas=(
 
 for formula in "${formulas[@]}"; do
     formula_name=$( echo "$formula" | awk '{print $1}' )
-    if brew cask list "$formula_name" > /dev/null 2>&1; then
+    if brew list "$formula_name" > /dev/null 2>&1; then
         echo "$formula_name already installed... skipping."
     else
         brew install "$formula"
@@ -37,7 +37,7 @@ brew tap caskroom/fonts
 
 for formula in "${cask_formulas[@]}"; do
     formula_name=$( echo "$formula" | awk '{print $1}' )
-    if brew list "$formula_name" > /dev/null 2>&1; then
+    if brew cask list "$formula_name" > /dev/null 2>&1; then
         echo "$formula_name already installed... skipping."
     else
         brew cask install "$formula"
